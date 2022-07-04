fac889d200e933115cc188ed8194fed4109c8fa6
Matias <matias_arenhard@hotmail.com>
2022-07-03 23:42:03 -0300

    chore: call business classes on controller

--- app/controllers/api/v1/social_medias_controller.rb
+++ app/controllers/api/v1/social_medias_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::SocialMediasController < ApplicationController
   def index
-    render json: {test: 'test'}, status: 200
+    social_medias_responses = {twitter: Twitter.new.take_twitters, facebook: Facebook.new.take_statuses, instagram: Instagram.new.take_photos}
+    render json: social_medias_responses, status: 200
   end
 end
