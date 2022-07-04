f08a2a07d4a02d339ca2bf2a233038a5eb778311
Matias <matias_arenhard@hotmail.com>
2022-07-03 23:35:56 -0300

    feat: create instagram business and spec

---
+++ app/business/instagram.rb
@@ -0,0 +1,22 @@
+class Instagram
+  include Api::V1::ApiHelper
+
+  def initialize
+    @url = ENV['URL_API_INSTAGRAM']
+    @timeout = ENV['URL_API_INSTAGRAM']
+  end
+
+  def take_photos
+    request = api_base.request_get(@url, @timeout)
+    if request[:status] == success_status
+      json_response = JSON(request[:response])
+      if json_response[0].key?('username')
+        json_response
+      else
+        random_error_message
+      end
+    else
+      request[:response]
+    end
+  end
+end
---
+++ spec/business/instagram_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Instagram do
+  context 'Instagram' do
+    let(:instagram_photos) {Instagram.new.take_photos[0]}
+    it 'Instagram take photos' do
+      expect(instagram_photos).to have_key('username')
+    end
+  end
+end
